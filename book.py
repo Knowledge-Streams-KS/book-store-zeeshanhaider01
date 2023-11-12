7c731a0a7f3a6412180eca4527a00fa15042dee8
Your Name <zeeshanhaider612@gmail.com>
2023-11-12 09:14:00 +0500

    feat/book.py/added a new file book.py

---
+++ book.py
@@ -0,0 +1,10 @@
+class book:
+
+    def __init__(self, title, author, genre, price, quantity):
+        self.title=title,
+        self.author=author,
+        self.genre=genre
+        self.price=price
+        self.quantity=quantity
+    
+    
